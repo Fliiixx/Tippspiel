04a4fa13df90dbbe4d9fd62ea7db64ea1f119942
Piscator <142930528+Fliiixx@users.noreply.github.com>
2025-10-18 13:37:47 +0200

    Change default redirect route to 'ergebnisse'

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { TippEingabeComponent } from './tipp-eingabe/tipp-eingabe.component';
 import { ErgebnisseComponent } from './ergebnisse/ergebnisse.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/tipp-eingabe', pathMatch: 'full' },
+  { path: '', redirectTo: '/ergebnisse', pathMatch: 'full' },
   { path: 'tipp-eingabe', component: TippEingabeComponent },
   { path: 'ergebnisse', component: ErgebnisseComponent },
 ];
