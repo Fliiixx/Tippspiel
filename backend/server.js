5494048069d72e6f8e5369107d09f56da79f7633
Piscator <142930528+Fliiixx@users.noreply.github.com>
2025-10-18 15:39:53 +0200

    Fix database path in server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 // Datenbankverbindung aufbauen
 const dbPromise = open({
-  filename: './data/database.sqlite',
+  filename: '/data/database.sqlite',
   driver: sqlite3.Database,
 });
 
